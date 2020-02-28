7c61bca522533139c6dc19dae450c1da9f478209
joy <joy.lai@zg.com>
2020-02-28 14:06:35 +0800

    add

--- src/log4js.test.ts
+++ src/log4js.test.ts
@@ -1,7 +1,8 @@
-import 'js-node-assist'
 import * as assert from 'assert'
 import Log4js from './log4js'
 import TimeUtil from '@pefish/js-util-time'
+import ErrorHelper from '@pefish/js-error';
+import util from 'util'
 
 describe('log4js', () => {
 
@@ -14,6 +15,8 @@ describe('log4js', () => {
   it('info', async () => {
     try {
       helper.info(`1111`, `2222`)
+      helper.error(`221`)
+      helper.error(util.inspect(new ErrorHelper(`123`)))
       await TimeUtil.sleep(4000)
     } catch (err) {
       global.logger.error(err)
