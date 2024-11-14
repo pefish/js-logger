204b1a2307016d16c3bf26e9bde0944706d3ac41
pefish <darrondarron@qq.com>
2024-11-14 16:31:42 +0800

    add

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,41 @@
-declare global {
-  namespace NodeJS {
-    interface Global {
-      logger: any,
-    }
-  }
-}
-import GcloudLogging from './gcloud_logging'
-import Log4js from './log4js'
+import * as log4js from "log4js";
 
-export {
-  GcloudLogging,
-  Log4js
+export interface ILogger {
+  getLevel(): string;
+
+  debug(msg: string): void;
+  info(msg: string): void;
+  warn(msg: string): void;
+  error(msg: string): void;
+}
+
+export class Logger implements ILogger {
+  private level: string;
+  private log4js: log4js.Logger;
+
+  constructor() {
+    this.level = process.env["LOG_LEVEL"];
+    log4js.configure({
+      appenders: { console: { type: "console" } },
+      categories: { default: { appenders: ["console"], level: this.level } },
+    });
+    this.log4js = log4js.getLogger();
+  }
+
+  getLevel(): string {
+    return this.level;
+  }
+
+  debug(msg: string): void {
+    this.log4js.debug(msg);
+  }
+  info(msg: string): void {
+    this.log4js.info(msg);
+  }
+  warn(msg: string): void {
+    this.log4js.warn(msg);
+  }
+  error(msg: string): void {
+    this.log4js.error(msg);
+  }
 }
--- tests/test.ts
+++
@@ -1,8 +0,0 @@
-
-import { Log4js } from '../src/index'
-import * as util from "util";
-import TimeUtil from "@pefish/js-util-time"
-
-const logger = new Log4js("aa", "./log/global", "info")
-
-logger.info("123", "456", 789)
